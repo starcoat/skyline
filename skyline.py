831abb9e7d89f3670c3772c3f35fc1955e5b7c2e
Christian Zieske <me@ekseiz.com>
2021-08-19 22:32:31 +0200

    Signatures of helper functions

--- skyline.py
+++ skyline.py
@@ -3,3 +3,17 @@ buildings = [
     ((60,160), (120,60), (120,250), (60,250)),
     ((-400,200), (-300,200), (-300,300), (-400,300))
 ]
+
+
+def calc_distance(corner: tuple[int, int]) -> int:
+    return 0
+
+
+# Gibt es eine schönere Alternative für diese Signatur:
+# corners: tuple[tuple[int, int], tuple[int, int], tuple[int, int], tuple[int, int]]
+def closest_corner_distance(corners) -> float:
+    return 0
+
+
+def max_height(distance: float) -> int:
+    return 0
